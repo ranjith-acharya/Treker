45fdb5ab7470430a97c3054e01bddbcf1b5b1559
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2025-04-06 12:21:31 +0530

    Create Server JS File

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+require('./models');
+
+// Load env variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
